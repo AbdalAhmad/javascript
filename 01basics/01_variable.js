bc0de5da113cc3b4d528fe420583e8f4c51e28c5
Lucky <abdalahmadm@Abdals-MacBook-Air.local>
2023-10-12 21:36:15 +0530

    add variable

---
+++ 01basics/01_variable.js
@@ -0,0 +1,24 @@
+const accountId = 234;
+let accountEmail ="abdalraza251@gmail.com";
+var accounPassword = "123423";
+
+accountCity = 'Delhi'
+
+// run = control +option+n
+// refersh = command + r
+//terminal = control +shift+ `
+
+// accountId = 2 //not allowed
+
+accountEmail = "abdal@gmail.com";
+accounPassword = "234234";
+accountCity = "Uttar Pradesh";
+
+let accounState;
+
+/*
+prefer not to use var because issue in block scope and function scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail,accounPassword, accountCity, accounState]);
