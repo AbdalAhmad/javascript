8513e1d069fb77bb9b55feb12f5dea58fe7aa306
Lucky <abdalahmadm@Abdals-MacBook-Air.local>
2023-10-13 11:06:43 +0530

    dataype added

---
+++ 01basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all Js code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Abdal");
+
+let name = "Abdal"
+let number = 23
+let isloggedin =false
+
+// number is have range = 2 to power 53
+// big int is also used for a  big number 
+// string = ''
+// boolean = true  or false
+// null = stand alone value null is not empty (null)
+// undefined  = to assign a value abdal;
+// symbol = for a uniqueness we use a symbol
+
+// object
+
+console.log(typeof ("Abdal")); // what is type of data type can be asked 
+console.log(typeof(null)); // it will give a object type 
+console.log(typeof(undefined)); // it willl show type undefined
