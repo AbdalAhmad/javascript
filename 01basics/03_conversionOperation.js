5d66f26766d2871ca7dd3b3ea410ec17c8b5bfba
Lucky <abdalahmadm@Abdals-MacBook-Air.local>
2023-10-13 11:48:04 +0530

    conversionOperation added

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+//  let score = null
+// console.log(typeof score); // number
+// console.log(typeof(score)); // string
+
+// convert value
+
+// let valueinNumber = Number(score) 
+// console.log(typeof (valueinNumber)); // number
+
+// console.log(valueinNumber);   // 23ad  it will give NaN nota number
+// console.log(typeof (valueinNumber)); // number // for null it give after convert 0 
+
+// converting to a Number
+// '33' == 33
+// '233za' == NaN
+// true = 1, false = 0
+// null = 0 
+// undefined = NaN
+
+// let islogged = 1
+// let islogged = ''
+// let converttobool = Boolean(islogged)
+// console.log(converttobool)  //  1 true, for  '' false on empty, for 0 false
+
+// let stringconvert ="iloveyou" // NaN
+// let convertstring = Number(stringconvert)
+// console.log(convertstring);
+
+let Numberc = 23
+let convertString = String(Numberc)
+console.log(convertString);  // string
+console.log(typeof (convertString)); // string
